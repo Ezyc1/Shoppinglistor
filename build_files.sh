2172b406228f747c1438ceaf0f86ca0c186241a6
Leonard.Cristescu <60926@gymnasium.ax>
2024-10-30 10:24:49 +0200

    Vercel ready

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-# build_files.sh
 
-# Exit immediately if a command exits with a non-zero status
-set -e
+echo "Installing requirements..."
+python3 -m pip install -r requirements.txt
 
-# Install any Python dependencies (if necessary)
-pip install -r requirements.txt
+echo "Running migrations..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
-# Collect static files for Django
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
-
-# Apply database migrations
-echo "Applying database migrations..."
-python3 manage.py migrate
--- shoppinglistor/settings.py
+++ shoppinglistor/settings.py
@@ -52,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'shoppinglistor.urls'
@@ -120,9 +119,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "/static")]
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
