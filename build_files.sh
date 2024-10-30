874cbbddc740ff37b289991d68d2c3d5393facd8
Leonard.Cristescu <60926@gymnasium.ax>
2024-10-30 09:13:13 +0200

    Work

--- build_files.sh
+++ build_files.sh
@@ -4,10 +4,13 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Install any Python dependencies (if necessary)
+pip install -r requirements.txt
+
 # Collect static files for Django
 echo "Collecting static files..."
 python3 manage.py collectstatic --noinput
 
 # Apply database migrations
-echo "Applying migrations..."
+echo "Applying database migrations..."
 python3 manage.py migrate
